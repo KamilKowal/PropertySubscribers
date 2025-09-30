c2f01bcb95aaa5f636456eaf5482528f0202b099
Kamil <kowalek1995@wp.pl>
2025-09-30 17:54:39 +0200

    Initial commit

---
+++ subscribers/core/models.py
@@ -0,0 +1,38 @@
+from django.db import models
+
+
+class Subscriber(models.Model):
+    create_date = models.DateTimeField(auto_now_add=True)
+    email = models.EmailField(unique=True)
+    gdpr_consent = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.email
+
+
+class SubscriberSMS(models.Model):
+    create_date = models.DateTimeField(auto_now_add=True)
+    phone = models.CharField(max_length=20, unique=True)
+    gdpr_consent = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.phone
+
+
+class Client(models.Model):
+    create_date = models.DateTimeField(auto_now_add=True)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=20)  # nie unique
+
+    def __str__(self):
+        return f"{self.email} ({self.phone})"
+
+
+class User(models.Model):
+    create_date = models.DateTimeField(auto_now_add=True)
+    email = models.EmailField()  # nie unique
+    phone = models.CharField(max_length=20)
+    gdpr_consent = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.email
